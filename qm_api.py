7c2d8ac1c4519b0af8b3081c697b364a26859ae2
TheLeetTaco <tboch3@gmail.com>
2024-06-09 17:26:44 -0400

    Cleaning up the api piece to be modular

--- qm_api.py
+++ qm_api.py
@@ -1,30 +1,37 @@
-from collections import defaultdict
-import matplotlib.pyplot as plt
-from qm_dataclass import Data
+from qm_dataclass import Quantmage_Data
 import requests
 import json
 
-# Define the endpoint identifier
+class QuantmageAPI:
+    def __init__(self, endpoint_id):
+        self.endpoint_id = endpoint_id
+        self.url = f'https://quantmage.app/grimoire/backtest/{self.endpoint_id}'
+        self.data_file = f'{self.endpoint_id}.json'
+        self.response_data = None
+        self.fetch_data()
+        self.data = self.load_data()
+
+    def fetch_data(self):
+        # Make the POST request
+        response = requests.post(self.url)
+        
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the JSON response
+            self.response_data = response.json()
+            
+            print("Data has been collected")
+        else:
+            print(f"Request failed with status code {response.status_code}")
+
+    def save_data(self):
+        with open(self.data_file, 'w') as json_file:
+            json.dump(self.data, json_file)
+
+    def load_data(self):
+        return Quantmage_Data(self.data_file)
+
+# Example usage
 endpoint_id = 'fc1a05b33b8e2a170584844265dfd998'
-
-# Construct the API endpoint URL
-url = f'https://quantmage.app/grimoire/backtest/{endpoint_id}'
-
-# Make the POST request
-response = requests.post(url)
-
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the JSON response
-    data = response.json()
-    
-    # Save the data to a JSON file
-    with open('data.json', 'w') as json_file:
-        json.dump(data, json_file)
-    
-    print("Data has been saved to data.json")
-else:
-    print(f"Request failed with status code {response.status_code}")
-
-data_obj = Data('data.json')
+quantmage_output = QuantmageAPI(endpoint_id)
 
--- qm_dataclass.py
+++ qm_dataclass.py
@@ -24,14 +24,15 @@ class Quantmage_Data:
     def from_json(obj: Any) -> 'Quantmage_Data':
         _value_history = obj.get("value_history")
         
-        # Format dates from YYYYMMDD to YYYY_MM_DD
-        raw_dates = obj.get("dates", [])
+        raw_dates = obj.get("dates")
+        # Selects the dates the algo uses
         raw_dates = raw_dates[raw_dates.index(max(obj.get("data_ranges"))):]
+        # Format dates from YYYYMMDD to YYYY_MM_DD
         _dates = [datetime.strptime(str(date), "%Y%m%d").strftime("%Y_%m_%d") for date in raw_dates]
         
         _length_of_backtest = len(_dates)
-        _allocation_history = [[Allocation(*allocation) for allocation in sublist] for sublist in obj.get("allocation_history", [])]
-        _visited_leaves_history = obj.get("visited_leaves_history", [])
+        _allocation_history = [[Allocation(*allocation) for allocation in sublist] for sublist in obj.get("allocation_history")]
+        _visited_leaves_history = obj.get("visited_leaves_history")
         
         # Extract other fields
         known_fields = {"value_history", "dates", "allocation_history", "visited_leaves_history"}
@@ -44,21 +45,3 @@ class Quantmage_Data:
         with open(file_path, 'r') as file:
             data = json.load(file)
         return Quantmage_Data.from_json(data)
-
-# # Prepare data for plotting value history with dates
-# dates_length = len(data_obj.dates)
-# value_history = data_obj.value_history[:dates_length]
-# dates = data_obj.dates[:dates_length]
-
-# # Plot value history with dates
-# plt.figure(figsize=(12, 6))
-# plt.plot(dates, value_history, label='Value History')
-
-# plt.title('Value History Over Time')
-# plt.xlabel('Date')
-# plt.ylabel('Value')
-# plt.xticks(rotation=45)
-# plt.legend()
-# plt.grid(True)
-# plt.tight_layout()
-# plt.show()
