cbd2be8d38e16224361bf2a6067a49a6feec6e8a
TheLeetTaco <tboch3@gmail.com>
2024-06-09 17:16:51 -0400

    Making a method to load in via a file from within the dataclass

--- qm_dataclass.py
+++ qm_dataclass.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
 from dataclasses import dataclass, field
 from typing import List, Any, Dict
+import matplotlib.pyplot as plt
 from datetime import datetime
 import json
 
@@ -12,7 +12,7 @@ class Allocation:
     value2: float
 
 @dataclass
-class Data:
+class Quantmage_Data:
     value_history: List[float] = field(default_factory=list)
     dates: List[str] = field(default_factory=list)
     allocation_history: List[List[Allocation]] = field(default_factory=list)
@@ -21,7 +21,7 @@ class Data:
     other_fields: Dict[str, Any] = field(default_factory=dict)
 
     @staticmethod
-    def from_dict(obj: Any) -> 'Data':
+    def from_json(obj: Any) -> 'Quantmage_Data':
         _value_history = obj.get("value_history")
         
         # Format dates from YYYYMMDD to YYYY_MM_DD
@@ -37,15 +37,13 @@ class Data:
         known_fields = {"value_history", "dates", "allocation_history", "visited_leaves_history"}
         _other_fields = {k: v for k, v in obj.items() if k not in known_fields}
         
-        return Data(_value_history, _dates, _allocation_history, _visited_leaves_history, _length_of_backtest, _other_fields)
+        return Quantmage_Data(_value_history, _dates, _allocation_history, _visited_leaves_history, _length_of_backtest, _other_fields)
 
-# Load the JSON data from the file
-with open('D:\Git Repos\quantmage_api\data.json') as json_file:
-    json_data = json.load(json_file)
-
-# Create the Data object from the JSON data
-data_obj = Data.from_dict(json_data)
-
+    @staticmethod
+    def from_json_file(file_path: str) -> 'Quantmage_Data':
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return Quantmage_Data.from_json(data)
 
 # # Prepare data for plotting value history with dates
 # dates_length = len(data_obj.dates)
